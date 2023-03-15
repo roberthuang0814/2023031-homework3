882c53987883255e29935593872f96641c494869
rober <rober@LAPTOP-9BJTS009>
2023-03-15 20:31:13 +0800

    加入專案檔案。

---
+++ 20230315題目3判斷座標是否在圓形的範圍內/20230315題目3判斷座標是否在圓形的範圍內.cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>  
+using namespace std;
+
+int main()
+{
+
+    int m, n;
+    while (cin >> m >> n) {
+        int Preset_radius = 10000;
+        double new_radius = (m * m) + (n * n); //設double型態確保   
+        if (new_radius < Preset_radius) {
+            cout << "inside\n";
+        }
+        else {
+            cout << "outside\n";
+        }
+
+    }
+
+    return 0;
+}
